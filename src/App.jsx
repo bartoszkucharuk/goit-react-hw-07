e6883beffaf88ca55cbacfeaa2275b7dd114d60a
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2025-02-10 14:20:16 +0100

    asyncThunk

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,37 @@ import { RiContactsBook3Line } from "react-icons/ri";
 import ContactForm from "./components/ContactForm";
 import SearchBox from "./components/SearchBox";
 import ContactList from "./components/ContactList";
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchContacts } from './redux/operations';
 
 
 function App() {
+  const dispatch = useDispatch();
+  const { items, isLoading, error } = useSelector(getContacts);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+
   return (
     <>
       <h1>Phonebook <RiContactsBook3Line size="40" /></h1>
       <ContactForm />
       <SearchBox />
-      <ContactList />
+      {loading && !error && (
+          <p>Please wait...</p>
+      )}
+      {error ? (
+          <p>{error}</p>
+      ) : contacts.length > 0 ? (
+        <ContactList />
+      ) : (
+        !loading && (
+            <p>You have no contacts yet</p>
+        )
+      )}
     </>
   )
 }
--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,15 +1,12 @@
 import axios from "axios";
 import { fetchingInProgress, fetchingSuccess, fetchingError } from "./contactsSlice";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
 
 axios.defaults.baseURL = "https://67a9e21165ab088ea7e4d460.mockapi.io/";
 
-export const fetchContacts = () => async dispatch => {
-    try {
-        dispatch(fetchingInProgress());
+export const fetchTasks = createAsyncThunk("contacts/fetchAll",
+    async () => {
         const response = await axios.get("/contacts");
-        dispatch(fetchingSuccess(response.data));
-    } catch (e) {
-        dispatch(fetchingError(e.message));
-    }
-};
+        return response.data;
+    });
