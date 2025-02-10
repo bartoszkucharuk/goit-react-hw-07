15cdfbd30a3cf74ef04b3ebfbe0d2f642e650715
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2025-02-10 14:52:47 +0100

    contactsSlice

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchContacts } from "./operations";
 
 
 const contactsInitialState = {
@@ -10,20 +11,22 @@ const contactsInitialState = {
 const contactsSlice = createSlice({
     name: "contacts",
     initialState: contactsInitialState,
-    reducers: {
-        fetchingInProgress(state) {
-            state.isLoading = true;
-        },
-        fetchingSuccess(state, action) {
-            state.isLoading = false;
-            state.error = null;
-            state.items = action.payload;
-        },
-        fetchingError(state, action) {
-            state.isLoading = false;
-            state.error = action.payload;
-        },
+  
+    extraReducers: builder => {
+        builder
+            .addCase(fetchContacts.pending, (state, action) => {
+                state.isLoading = true;
+             })
+            .addCase(fetchContacts.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.error = null;
+                state.items = action.payload;
+             })
+            .addCase(fetchContacts.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+             });
     },
 });
 
-export const { fetchingInProgress, fetchingSuccess, fetchingError } = contactsSlice.actions;
+export const contactsReducer = contactsSlice.reducer;
--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,12 +1,17 @@
 import axios from "axios";
-import { fetchingInProgress, fetchingSuccess, fetchingError } from "./contactsSlice";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
 
 axios.defaults.baseURL = "https://67a9e21165ab088ea7e4d460.mockapi.io/";
 
-export const fetchTasks = createAsyncThunk("contacts/fetchAll",
-    async () => {
-        const response = await axios.get("/contacts");
-        return response.data;
-    });
+export const fetchContacts = createAsyncThunk(
+    "contacts/fetchAll",
+    async (_, thunkAPI) => {
+        try {
+            const response = await axios.get("/contacts");
+            return response.data;
+        } catch (e) {
+            return thunkAPI.rejectWithValue(e.message);
+        }
+    }
+);
